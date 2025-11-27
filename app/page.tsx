54cb3df6831f1e665c1eb9939fa6af5c5dd8620a
mame77 <morinaga.kota@icloud.com>
2025-11-27 20:26:49 +0900

    refact

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CatImage } from "./cat-image";
 import { fetchImage } from "./fetch-images";
 
+
 export default async function Home() {
     const image = await fetchImage();
     return <CatImage url={image.url} />;
